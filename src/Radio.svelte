c69d336b171cdd983182ccf85662d209ee8bc412
Mikha Davids <mikhadavids@gmail.com>
2022-03-26 23:04:47 +0200

    refactor: :fire: Made radio button into a single component

--- src/Radio.svelte
+++ src/Radio.svelte
@@ -1,30 +1,36 @@
 <script lang="ts" context="module">
-	import Radio from "./_Radio.svelte";
+	type RadioObj = {
+		value: string;
+		uncheck: () => void;
+		isChecked: () => boolean;
+	};
+
 	class Group {
-		private elements: Set<Radio>;
+		private elements: Set<RadioObj>;
 		public value: string;
 		public size: number;
 		constructor() {
-			this.elements = new Set<Radio>();
+			this.elements = new Set<RadioObj>();
 			this.value = null;
 			this.size = 0;
 		}
-		public add(element: Radio) {
-			if (element.checked) this.check(element);
+		public add(element: RadioObj) {
+			if (element.isChecked()) this.check(element);
 			this.elements.add(element);
 			++this.size;
 		}
-		public delete(element: Radio) {
+		public delete(element: RadioObj) {
 			this.elements.delete(element);
 			--this.size;
-			if (element.checked) this.value = null;
+			if (element.isChecked()) this.value = null;
 		}
-		public check(element: Radio) {
+		public check(element: RadioObj) {
 			this.value = element.value;
 			for (const el of this.elements) {
 				// Checking first because setting it may cause extra updates even if it doesn't change
-				if (el.checked && el !== element) {
-					el.checked = false;
+				console.log(el.isChecked());
+				if (el.isChecked() && el !== element) {
+					el.uncheck();
 				}
 			}
 		}
@@ -36,10 +42,15 @@
 <script lang="ts">
 	import { onDestroy, onMount } from "svelte";
 
+	export let checked = false;
 	export let name: string = null;
 	export let value: string;
-	export let checked = false;
-	let self: Radio;
+
+	const self: RadioObj = {
+		value,
+		uncheck: () => (checked = false),
+		isChecked: () => checked,
+	};
 
 	$: if (checked && name) groups.get(name).check(self);
 
@@ -60,6 +71,49 @@
 	});
 </script>
 
-<Radio bind:this={self} {name} {value} bind:checked>
-	<slot />
-</Radio>
+<div class="radio" on:click={() => (checked = true)} {checked}>
+	<span class="button" />
+	<span class="label">
+		<slot />
+	</span>
+</div>
+
+<style lang="scss">
+	.radio {
+		cursor: pointer;
+		display: flex;
+		gap: 0.5rem;
+		align-items: center;
+	}
+	.button {
+		display: inline-block;
+		height: 1em;
+		aspect-ratio: 1;
+		border-radius: 50%;
+		position: relative;
+		&::before {
+			visibility: hidden;
+		}
+		&::before,
+		&::after {
+			content: "";
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+			position: absolute;
+			border: solid 0.2rem;
+			transition: all 0.2s ease-in-out;
+		}
+	}
+	.radio[checked="true"] {
+		.button::after {
+			border-color: teal;
+			transform: scale(0.5);
+			background-color: teal;
+		}
+		.button::before {
+			visibility: visible;
+			border-color: teal;
+		}
+	}
+</style>
